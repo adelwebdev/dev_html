d3133560783019bac8b1e924dc0759113c3fae11
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2023-05-01 16:16:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 let rr = "une lettre";
+
+const cst = document.querySelector("h1");
+cst.addEventListener('click', (e) => {
+    console.log('YESSSSS');
+    console.log(e);
+})
+
+const cst2 = document.querySelector("h1");
+cst2.addEventListener('mousemove', (e) => {
+    console.log("ca bouge les amis!")
+    console.log(e);
+})
+
+h1.classList.remove(104);
